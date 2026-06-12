7a33cc5e00c19a960b756a55147b6a54a017179b
Halmein <henrique.ross@protonmail.com>
2026-06-12 18:56:54 -0300

    Changes to be committed: 	new file:   engText.txt 	modified:   index.html 	modified:   languageButton.js 	new file:   ptbrText.txt

--- languageButton.js
+++ languageButton.js
@@ -1,14 +1,39 @@
 let btnChangeLanguage = document.getElementById("languageBtn");
 let currentLanguage = "pt-br";
-let p = document.querySelector("p");
+let buttonText = document.querySelector("p");
+let textResponse = '';
+let text = document.getElementById("text");
+
+async function fetchText() {
+    try {
+        if (currentLanguage === "en") {
+            const response = await fetch("textEng.txt");
+            textResponse = await response.text();
+            text.textContent = textResponse;
+            log("Text fetched successfully:", textResponse);
+        }
+        else {
+            const response = await fetch("textPtBr.txt");
+            textResponse = await response.text();
+            text.textContent = textResponse;
+            log("Text fetched successfully:", textResponse);
+        }
+    }
+    catch (error) {
+        console.error("Error fetching text:", error);
+    }
+}
 
 btnChangeLanguage.addEventListener("click", () => {
     if (currentLanguage === "pt-br") {
-        p.textContent = "ENG";
+        buttonText.textContent = "ENG";
         currentLanguage = "en";
+        fetchText();
     }
     else {
-        p.textContent = "PTBR";
+        buttonText.textContent = "PTBR";
         currentLanguage = "pt-br";
+        fetchText();
     }
+
 });
